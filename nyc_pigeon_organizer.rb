e1b2e85bd8d336853ecd2752985829977a469f1b
Scott Hoisington <hoyzington@gmail.com>
2019-11-17 01:13:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,67 @@
+
+def remove_key_hash_of_hoa(hash)
+  new = []
+  hash.each {|key, value|                                #  <--  pigeon_data
+    new << value
+  }                                                      #  -->  remove_first_key
+  new
+end
+
+
+def remove_key_array_of_hoa(array)
+  new = []
+  i = 0
+  while i < array.size
+    new[i] = []
+    array[i].each {|key, value|                          #  <--  remove_first_key
+      new[i] << value
+    }                                                    #  -->  names_aoa
+    i += 1
+  end
+  new
+end
+
+
+def simplify_aoa(array)
+  new1 = array.flatten                                   #  <--  names_aoa
+  new2 = new1.uniq
+  new2                                                   #  -->  names_final_array
+end
+
+
+def stringify(array)
+  new = []
+  i = 0
+  while i < array.size
+    new[i] = array[i].map{|key, value|                   #  <--  remove_first_key
+      key.to_s
+    }                                                    #  -->  string_keys
+    i += 1
+  end
+  new
+end
+
+
+def new_hash(names, instances, traits)
+  new = {}
+  i = 0
+  while i < names.size                                   #  <--  names_final_array
+    while ii < instances.size
+    new[names[i]] = {color: [], gender: [], lives: []}
+    new[names[i]][:color] << traits[i][ii]
+    new[names[i]][:gender] << traits[i][ii]
+    new[names[i]][:lives] << traits[i][ii]
+    i += 1                                               #  -->  pigeon_list
+  end
+  new
+end
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  remove_first_key = remove_key_hash_of_hoa(data)
+  names_aoa = remove_key_array_of_hoa(remove_first_key)
+  names_final_array = simplify_aoa(names_aoa)
+  string_keys = stringify(remove_first_key)
+  pigeon_list = new_hash(names_final_array)
+  pigeon_list
 end
